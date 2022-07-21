a1b7789230366b037f634b52d0091283ce5ed95b
wyylame <w1llhorn@yandex.ru>
2022-07-21 21:55:18 +0300

    create main components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import "./App.css";
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import PizzaItem from "./components/PizzaItem";
+import Sort from "./components/Sort";
+
+import './scss/app.scss'
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div class="wrapper">
+        <Header/>
+        <div class="content">
+          <div class="container">
+            <div class="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 class="content__title">Все пиццы</h2>
+            <div class="content__items">
+              <PizzaItem/>
+              <PizzaItem/>
+              <PizzaItem/>
+              <PizzaItem/>
+              <PizzaItem/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
