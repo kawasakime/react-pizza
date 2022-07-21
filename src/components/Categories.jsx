e51d14d4a479c692e47f4566232795bf445ca948
wyylame <w1llhorn@yandex.ru>
2022-07-21 22:10:20 +0300

    edit catecories component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 const Categories = () => {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+  const [active, setActive] = useState(0);
+
+  const setActiveHandler = (i) => {
+    setActive(i);
+  };
+
   return (
     <div class="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((title, i) => (
+          <li
+            key={i}
+            className={active === i ? "active" : null}
+            onClick={() => setActiveHandler(i)}
+          >
+            {title}
+          </li>
+        ))}
       </ul>
     </div>
   );
